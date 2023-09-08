95d1682e8ca5e5a873c2957fa39b4cfaf41b2e42
RoadRach <70367556+RoadRach@users.noreply.github.com>
2023-09-08 15:39:47 +0800

    added text wrap, font size change

--- lovebonitoscraper/lovebonitoscraper/main.py
+++ lovebonitoscraper/lovebonitoscraper/main.py
@@ -1,10 +1,28 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
+from textwrap import wrap
 
 lb_data = pd.read_csv('lovebfile.csv')
-print(lb_data)
+
+x = lb_data['name']
+x= [ '\n'.join(wrap(l, 20)) for l in x ]
+y = lb_data['price']
+# print("before for loop")
+# for line in lb_data:
+#     x.append(str(line[0]))
+#     print('x')
+#     y.append(str(line[1]))
+
+# print(x)
+# print(y)
 # print(lb_data.to_string())
 print("hello world")
 
 # price = sns.load_dataset(lb_data)
-sns.histplot(data=lb_data, x="name", multiple="stack")
+# sns.histplot(data=lb_data, x="name", y="price", multiple="stack")
+
+plt.plot(x, y, 'g*')
+plt.xticks(fontsize=5)
+
+plt.show()
